c26129741164dba40c1fa49c69879465429f978b
Fernando Álvarez <alvarez.calleja.fernando@gmail.com>
2014-08-08 12:40:28 +0200

    Add godep support

---
+++ Godeps/_workspace/src/code.google.com/p/go-html-transform/h5/vars.go
@@ -0,0 +1,66 @@
+package h5
+
+var (
+	// Scopes
+	allScope  = map[string]bool{}
+	baseScope = map[string]bool{
+		"applet":         true,
+		"caption":        true,
+		"html":           true,
+		"table":          true,
+		"td":             true,
+		"th":             true,
+		"marquee":        true,
+		"object":         true,
+		"mi":             true,
+		"mo":             true,
+		"mn":             true,
+		"ms":             true,
+		"mtext":          true,
+		"annotation-xml": true,
+		"foreignObject":  true,
+		"desc":           true,
+		"title":          true,
+	}
+	buttonScope = map[string]bool{
+		"applet":         true,
+		"caption":        true,
+		"html":           true,
+		"table":          true,
+		"td":             true,
+		"th":             true,
+		"marquee":        true,
+		"object":         true,
+		"mi":             true,
+		"mo":             true,
+		"mn":             true,
+		"ms":             true,
+		"mtext":          true,
+		"annotation-xml": true,
+		"foreignObject":  true,
+		"desc":           true,
+		"title":          true,
+		"button":         true,
+	}
+	listScope = map[string]bool{
+		"applet":         true,
+		"caption":        true,
+		"html":           true,
+		"table":          true,
+		"td":             true,
+		"th":             true,
+		"marquee":        true,
+		"object":         true,
+		"mi":             true,
+		"mo":             true,
+		"mn":             true,
+		"ms":             true,
+		"mtext":          true,
+		"annotation-xml": true,
+		"foreignObject":  true,
+		"desc":           true,
+		"title":          true,
+		"ol":             true,
+		"ul":             true,
+	}
+)
